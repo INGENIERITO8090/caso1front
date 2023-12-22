707ee3141b3f64d65b9b1880c510b41dd434676d
Camila Arias <ariasruizcamilaa@gmail.com>
2023-12-22 14:28:21 -0500

    front api

---
+++ vue-folder/src/components/List-client.vue
@@ -0,0 +1,36 @@
+
+<template>
+<div>
+
+</div>
+</template>
+<script>
+
+
+import axios from "axios";
+
+export default {
+
+   name:"List_client" +
+       "" ,
+  data(){
+     return{
+       clientes:[]
+
+     }
+  },
+
+async  created() {
+
+
+   let url = "http://localhost:8080/cliente/all"
+
+  axios.get(url).then(response => console.log(response))
+   }
+};
+</script>
+<style scoped>
+
+
+</style>
+
